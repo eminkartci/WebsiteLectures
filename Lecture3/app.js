cb52f4975fdf2299b16164bed09400f155b1fc79
eminkartci <emin.kartci@ozu.edu.tr>
2021-07-31 22:06:12 +0300

    Variables

--- Lecture3/app.js
+++ Lecture3/app.js
@@ -1,1 +1,11 @@
-console.log('Hello Ezgim!')
+
+
+// Variables
+let number1 = 123532;
+let number2 = 245;
+
+let sum = number1 + number2;
+const eminem = 1345;
+
+// Output
+console.log("Sum: ",sum)
