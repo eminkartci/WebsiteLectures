d50eebcb393eee41545bafd6912218b299d69963
eminkartci <emin.kartci@ozu.edu.tr>
2021-07-31 22:09:01 +0300

    Functions are added

--- Lecture3/app.js
+++ Lecture3/app.js
@@ -8,4 +8,18 @@ let sum = number1 + number2;
 const eminem = 1345;
 
 // Output
-console.log("Sum: ",sum)
+// console.log("Sum: ",sum)
+
+// Functions
+function sumFunc(num1,num2){
+    let sum = num1+num2;
+    console.log("Sum: ",sum)
+}
+
+function mul(num1,num2){
+    let Multiply = num1 * num2;
+    console.log("Multiply: ",Multiply)
+}
+
+sumFunc(324,5321)
+mul(325,734)
